5b012a85fef4ce7fafda8cb5f50ea1c900e2dd62
Matt Freitas <mattfreitas415@gmail.com>
2019-01-15 15:43:26 -0800

    event attributes complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,48 +1,77 @@
 "use strict";
 
-// arguments object no longer bound with arrow functions
+console.log("App.js is running");
 
-// const add = function(a, b) {
-//   console.log(arguments);
-//   return a + b;
-// };
-// console.log(add(55, 1));
-
-// const addArrow = (a, b) => {
-//   // console.log(arguments); // <-- triggers error
-//   return a + b;
-// };
-
-// this keyword no longer bound with arrow functions
-
-var user = {
-  name: "Andrew",
-  cities: ["Philadelphia", "Oakland", "Atlanta"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + " has lived in " + city;
-    });
-  }
-};
-console.log(user.printPlacesLived());
-
-// Challenge Bitch
-
-var multiplier = {
-  // numbers - an array of numbers
-  // multiplyBy - single number
-  // multiply - return a new array where the numbers have been multiplied
-  numbers: [7, 8, 11, 44],
-  multiplyBy: 69,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var app = {
+  title: "Indecision App",
+  subtitle: "this is some info for the subtitle",
+  options: ["one", "two"]
 };
 
-console.log(multiplier.multiply());
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  app.options.length > 0 ? React.createElement(
+    "p",
+    null,
+    "\"yes options\""
+  ) : React.createElement(
+    "p",
+    null,
+    "\"no options\""
+  )
+);
+
+var count = 0;
+
+var addOne = function addOne() {
+  console.log("addOne");
+};
+
+var minusOne = function minusOne() {
+  console.log("minusOne");
+};
+
+var reset = function reset() {
+  console.log("reset");
+};
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Count: ",
+    count
+  ),
+  React.createElement(
+    "button",
+    { onClick: addOne },
+    "+1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: minusOne },
+    "-1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: reset },
+    "reset"
+  )
+);
+console.log(templateTwo);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(templateTwo, appRoot);
