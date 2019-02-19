65373fac2498cea06770241d6b6a3f31623416dc
Matt Freitas <mattfreitas415@gmail.com>
2019-02-18 22:29:37 -0800

    15-method-binding

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+const obj = {
+  name: "Vikram",
+  getName() {
+    return this.name;
+  }
+}
+
+const getName = obj.getName.bind(obj);
+
+console.log(getName());
+
 class IndecisionApp extends React.Component {
   render() {
     const title = 'Indecision';
@@ -40,8 +51,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
   handleRemoveAll() {
-    alert("Remove All!")
+    console.log(this.props.options)
   }
   render() {
     return (
